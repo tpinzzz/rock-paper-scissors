c2244bfa1876407857115cb06c280b18060a839c
Tyler <tpinzzz@users.noreply.github.com>
2023-08-23 09:01:11 -0400

    Write a function to act as the players opponent and randomly return rock, paper or scissors

--- rps.js
+++ rps.js
@@ -1,11 +1,35 @@
 
+function getComputerChoice() {
+    /*get a RandomInt between 0 and 2 inclusive*/
+    a = getRandomInt(3);
+    console.log(a);
+    if (a === 0) {
+        console.log('Rock');
+        return 'Rock';
+    }
+    else if (a === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
 function getRandomInt(max) {
     return Math.floor(Math.random()*max);
 }
 
+/*Write a function that plays a single round of Rock Paper
+Scissors. The function should take two parameters - the
+playerSelection and computerSelection - and then return a 
+string that declares the winner of the round like so: "You
+Lose! Paper beats Rock"*/
 
-function getComputerChoice() {
-    return 0
+function playRound (playerSelection, computerSelection) {
+    //propt player to insert rock, paper or scissors
+    playerSelection
+    
+    return 'You Lose! Paper beats Rock'
 }
 
-console.log(getRandomInt)
+console.log(getComputerChoice());
+
